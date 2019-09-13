788de07e98679b764d6127891f64b5442464661d
Glitch (hello-sqlite) <none>
2019-09-13 16:39:04 +0000

    🌂😂 Checkpoint ./bot.js:1076878/346

--- bot.js
+++ bot.js
@@ -11,10 +11,16 @@ client.on('message', (msg) => {
   if (msg.content.startsWith('?report')) {
     var ch = msg.guild.channels.find(ch => ch.name === 'reports');
     var user = msg.mentions.users.first();
-    var reason = 
+    var reason = msg.content
     var embed = new RichEmbed;
     embed.setTitle('New Report');
     embed.setAuthor(msg.member.displayName, msg.user.avatarURL);
-    embed.setDescription('Re')
+    embed.setDescription(reason);
+    embed.setFooter(process.env.footer);
+    ch.send(embed);
+  } else if (msg.content.startsWith('?close')) {
+    if (msg.channel.name.startsWith('ticket-')) {
+      msg.channel.delete();
+    }
   }
 });
