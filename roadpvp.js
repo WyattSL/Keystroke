3337f8e9444241f96a4979dd5f00fefddd13e1dc
Glitch (hello-sqlite) <none>
2019-09-13 21:51:09 +0000

    ⛈📺 Checkpoint ./roadpvp.js:1076878/1481

--- roadpvp.js
+++ roadpvp.js
@@ -8,19 +8,7 @@ client.on('ready', () => {
 
 client.on('message', (msg) => {
   if (msg.author.bot) return;
-  if (msg.content.startsWith('?report')) {
-    var ch = msg.guild.channels.find(ch => ch.name === 'reports');
-    msg.delete();
-    var user = msg.mentions.users.first();
-    var reason = msg.content
-    var embed = new RichEmbed;
-    embed.setTitle('New Report');
-    embed.setAuthor(msg.member.displayName, msg.user.avatarURL);
-    embed.setDescription(reason);
-    embed.setFooter(process.env.footer);
-    ch.send(embed);
-    msg.channel.send("Your report has been submitted.")
-  } else if (msg.content.startsWith('?close')) {
+  if (msg.content.startsWith('?close')) {
     if (msg.channel.name.startsWith('ticket-')) {
       msg.channel.delete();
       msg.guild.members.find(m => m.username === msg.channel.topic).send("Your ticket has been closed by " + msg.member.displayName + ".")
@@ -47,8 +35,23 @@ client.on('message', (msg) => {
     var tosay = msg.content.slice(5, msg.content.length);
     msg.delete();
     msg.channel.send(tosay)
+  } else if (msg.content.startsWith("?announce")) {
+    var tosay = msg.content.slice(10, msg.content.length);
+    msg.delete();
+    var ch = msg.channels.find(ch => ch.name === "announcements");
+    if (!ch) {
+      msg.guild.createChannel("announcements", { type: "text", topic: "News Updates!", nsfw: false });
+      msg.guild.owner.send('I have automatically created a text channel named "Announcements". Announcements will be sent there.');
+      var ch = msg.channels.find(ch => ch.name === "announcements");
+    };
+    var embed = new RichEmbed;
+    embed.setTitle('Announcement');
+    embed.setAuthor(msg.member.displayName, msg.author.avatarURL);
+    embed.setDescription(tosay);
+    embed.setFooter(process.env.FOOTER);
+    embed.setColor(0x000000);
   }
 });
 
 
-client.login(process.env.ZontServer);
+client.login(process.env.roadpvp);
