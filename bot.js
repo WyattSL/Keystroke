7c4e3028f3a329ea7a976115675e7bcd0483dcba
Glitch (hello-sqlite) <none>
2019-09-13 20:41:03 +0000

    🌊🚡 Checkpoint ./bot.js:1076878/739

--- bot.js
+++ bot.js
@@ -28,16 +28,24 @@ client.on('message', (msg) => {
       msg.channel.send("This is not an ticket channel!")
     }
   } else if (msg.content.startsWith("?new")) {
+    msg.delete();
     var tr = msg.guilds.roles.find(r => r.name === "TicketRole");
     if (!tr) {
       msg.guild.createRole({ name: "TicketRole" }, "Ticket Bot Setup");
       msg.guild.owner.send('I have automatically created a role named "TicketRole". This role should be given to staff members to view tickets.');
+      var tr = msg.guilds.roles.find(r => r.name === "TicketRole");
     }
     var tc = msg.guilds.channels.find(ch => ch.name === "tickets" && ch.type === "category");
     if (!tc) {
-      msg.guild.createChannel("tickets", {type: "category"})
+      msg.guild.createChannel("tickets", {type: "category"});
+      msg.guild.owner.send('I have automatically created a category channel named "Tickets". This channel is where all tickets will be stored.');
+      var tc = msg.guilds.channels.find(ch => ch.name === "tickets" && ch.type === "category");
     }
     msg.guild.createChannel("ticket-" + msg.author.username, { type: "text", topic: msg.author.username, nsfw: false});
-    
+    msg.author.send("Your ticket has been created. You can view it in the TICKETS category.");
+  } else if (msg.content.startsWith("?say")) {
+    var tosay = msg.content.slice()
+    msg.delete();
+    msg.channel.send("")
   }
 });
