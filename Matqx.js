d22669d1f890101150df21296fa426f0856b5afc
Glitch (hello-sqlite) <none>
2019-09-14 21:49:40 +0000

    ⛲️🚋 Checkpoint ./Matqx.js:1076878/6046

--- Matqx.js
+++ Matqx.js
@@ -8,19 +8,7 @@ client.on('ready', () => {
 
 client.on('message', (msg) => {
   if (msg.author.bot) return;
-  if (msg.content.startsWith('?report')) {
-    var ch = msg.guild.channels.find(ch => ch.name === 'reports');
-    msg.delete();
-    var user = msg.mentions.users.first();
-    var reason = msg.content
-    var embed = new RichEmbed;
-    embed.setTitle('New Report');
-    embed.setAuthor(msg.member.displayName, msg.user.avatarURL);
-    embed.setDescription(reason);
-    embed.setFooter(process.env.footer);
-    ch.send(embed);
-    msg.channel.send("Your report has been submitted.")
-  } else if (msg.content.startsWith('?close')) {
+  if (msg.content.startsWith('?close')) {
     if (msg.channel.name.startsWith('ticket-')) {
       msg.channel.delete();
       msg.guild.members.find(m => m.username === msg.channel.topic).send("Your ticket has been closed by " + msg.member.displayName + ".")
@@ -37,7 +25,7 @@ client.on('message', (msg) => {
     }
     var tc = msg.guilds.channels.find(ch => ch.name === "tickets" && ch.type === "category");
     if (!tc) {
-      msg.guild.createChannel("tickets", {type: "category"});
+      msg.guild.createChannel("tickets", "category", [{ allow: ["VIEW_CHANNEL", "SEND_MESSAGES"], id: `${tr.id}` }, {}]);
       msg.guild.owner.send('I have automatically created a category channel named "Tickets". This channel is where all tickets will be stored.');
       var tc = msg.guilds.channels.find(ch => ch.name === "tickets" && ch.type === "category");
     }
@@ -47,8 +35,51 @@ client.on('message', (msg) => {
     var tosay = msg.content.slice(5, msg.content.length);
     msg.delete();
     msg.channel.send(tosay)
+  } else if (msg.content.startsWith("?announce")) {
+    var tosay = msg.content.slice(10, msg.content.length);
+    msg.delete();
+    var ch = msg.channels.find(ch => ch.name === "announcements");
+    if (!ch) {
+      msg.guild.createChannel("announcements", { type: "text", topic: "News Updates!", nsfw: false });
+      msg.guild.owner.send('I have automatically created a text channel named "Announcements". Announcements will be sent there.');
+      var ch = msg.channels.find(ch => ch.name === "announcements");
+    };
+    var embed = new RichEmbed;
+    embed.setTitle('Announcement');
+    embed.setAuthor(msg.member.displayName, msg.author.avatarURL);
+    embed.setDescription(tosay);
+    embed.setFooter(process.env.FOOTER);
+    embed.setColor(0x000000);
+    ch.send(embed);
+  } else if (msg.content.startsWith("?kick")) {
+    var tu = msg.mentions.users.first();
+    var embed = new RichEmbed;
+    embed.setTitle('Punishment');
+    embed.setAuthor(msg.member.displayName, msg.author.avatarURL);
+    embed.setDescription('Kicking ' + tu) // xd
+    embed.setFooter(process.env.FOOTER);
+    embed.setColor(0xFF0000);
+    msg.channel.send(embed);
+    tu.kick();
+  } else if (msg.content.startsWith("?ban")) {
+    var tu = msg.mentions.users.first();
+    var embed = new RichEmbed;
+    embed.setTitle('Punishment');
+    embed.setAuthor(msg.member.displayName, msg.author.avatarURL);
+    embed.setDescription('Banning ' + tu) // xd
+    embed.setFooter(process.env.FOOTER);
+    embed.setColor(0xFF0000);
+    msg.channel.send(embed);
+    tu.ban();
   }
 });
 
+client.on('guildMemberAdd', (m) => {
+  var tr = m.guild.roles.find(r => r.name === "<!> Player <!>");
+  if (tr) {
+    m.addRole(tr, "AutoRank");
+  };
+});
 
-client.login(process.env.ZontServer);
+
+client.login(process.env.roadpvp);
