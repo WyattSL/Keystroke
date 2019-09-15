026a3155718753f98bc5448c79e35d276d453daf
Glitch (hello-sqlite) <none>
2019-09-15 22:39:22 +0000

    🚨📕 Checkpoint ./commands.json:1076878/347 ./global.js:1076878/546 ./Matqx.json:1076878/26 ./ZontServer.json:1076878/41 ./roadpvp.json:1076878/27 ./global.json:1076878/21

--- global.js
+++ global.js
@@ -1,4 +1,7 @@
+const { Client, RichEmbed } = require('discord.js');
+
 exports.ready = function(b, c) {
+  if (!b || !c) return false;
   const d = require(`./${b}.json`);
   var s = d.status;
   var m = d.mode;
@@ -9,4 +12,13 @@ exports.ready = function(b, c) {
   } else {
     console.log(`Keystroke Bot Ready | Logged in as ${c.user.username}#${c.user.discriminator} with ${c.guilds.array().length} the first one being ${c.guilds.first().name}`);
   };
+  return true;
 };
+
+exports.help = function(b) {
+  const d = require(`./${b}.json`);
+  if (!b) return false;
+  var e = new RichEmbed().setColor(0x000000)
+  .setFooter(`Job Request by ${d.owner}`)
+  .set
+}
