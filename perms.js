e236dce12ba30706620e5e5416dfe9cdcc250faa
Glitch (keystroke) <none>
2019-09-16 21:38:22 +0000

    🍐🎪 Checkpoint ./perms.js:1076878/412

---
+++ perms.js
@@ -0,0 +1,22 @@
+const { Client, RichEmbed } = require('discord.js');
+const sql = require('./mysql.js');
+const pl = require('./pl.json');
+
+exports.noPerm = function(ch) {
+  var e = new RichEmbed()
+  .setTitle(`Error`)
+  .setColor(0xFF0000)
+  .setFooter(process.env.FOOTER)
+  .setDescription(`You are lacking permission to perform this action!`);
+  ch.send(e);
+}
+
+exports.checkPerm = function(p, m, c) {
+  if (!m || !p) return false;
+  if (m.hasPermission(p, false, true, true)) {
+    return true;
+  } else {
+    this.noPerm(c);
+    return false;
+  }
+};
