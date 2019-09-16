b7421208a8a0ebe95c3330ae0b2b7943b2bd979f
Glitch (keystroke) <none>
2019-09-16 21:18:27 +0000

    🔑🌋 Checkpoint ./commands/kick.js:1076878/668 ./perms.js:1076878/10 ./permissions.json:1076878/197

---
+++ commands/kick.js
@@ -0,0 +1,18 @@
+//basic api functions
+const { Client, RichEmbed } = new RichEmbed;
+const sql = require('./mysql.js');
+const perms = require('./perms.js');
+
+
+exports.run = function(n, c, m) {
+  if (!perms.checkPerm(m.member, "KICK_MEMBERS", m.channel)) return;
+  var tu = m.mentions.members.first();
+  var embed = new RichEmbed;
+  embed.setTitle('Punishment');
+  embed.setAuthor(m.member.displayName, m.author.avatarURL);
+  embed.setDescription('Kicking ' + tu)
+  embed.setFooter(process.env.FOOTER);
+  embed.setColor(0xFF0000);
+  m.channel.send(embed);
+  tu.kick();
+};
