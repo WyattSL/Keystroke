5a2c282d604544693726f49250df6fd0193e52df
Glitch (keystroke) <none>
2019-09-22 18:09:17 +0000

    🚓🐘 Checkpoint ./commands/close.js:1076878/373 ./sneakyutilites.js:1076878/10 ./commands/ban.js:1076878/833

--- commands/ban.js
+++ commands/ban.js
@@ -7,10 +7,20 @@ const perms = require('./perms.js');
 exports.run = function(n, c, m) {
   if (!perms.checkPerm(m.member, "BAN_MEMBERS", m.channel)) return;
   var tu = m.mentions.members.first();
+  if (!tu) {
+    var tm = m.content.slice(5, m.content.length);
+    tu = m.guild.members.find(me => me.displayName === tm).array();
+    if (!tu) tu = m.guild.users.find(me => me.username === tm).array();
+  }
   var embed = new RichEmbed;
   embed.setTitle('Punishment');
   embed.setAuthor(m.member.displayName, m.author.avatarURL);
-  embed.setDescription('Banning ' + tu)
+  embed.setDescription('Banning ' + tu);
+  if (!tu) {
+    embed.setDescription(`Please mention an user, or type their nickname/displayname`)
+  } else if (tu.length > 1) {
+    embed.setDescription(`It appears`)
+  }
   embed.setFooter(process.env.FOOTER);
   embed.setColor(0xFF0000);
   m.channel.send(embed);
--- commands/close.js
+++ commands/close.js
@@ -5,5 +5,10 @@ const perms = require('./perms.js');
 
 
 exports.run = function(n, c, msg) {
-  if (!perms.checkPerm(msg.member, "KICK_MEMBERS", msg.channel)) return;
+  if (msg.channel.name.startsWith('ticket-')) {
+    msg.channel.delete();
+    msg.guild.members.find(m => m.user.username === msg.channel.topic).send("Your ticket has been closed by " + msg.member.displayName + ".")
+  } else {
+    msg.channel.send("This is not an ticket channel!")
+  }
 };
