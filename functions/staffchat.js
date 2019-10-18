7a2d482738d616ffe3788a2c7e427ebff0409bcc
Glitch (keystroke) <none>
2019-10-18 23:18:26 +0000

    👵🚪 Checkpoint ./functions/staffchat.js:1076878/779 ./bots/sneakyutilites.json:1076878/28

--- functions/staffchat.js
+++ functions/staffchat.js
@@ -1,7 +1,19 @@
 const express = require('express');
 const app = express();
+const Discord = require('discord.js');
 
 
 exports.chat = function(user, msg, id) {
-  
+  var bf = require(`./bots/${id}.json`);
+  if (!bf || !id || !user || !msg || !bf.scid || !bf.scto) return false;
+  var client = new Discord.WebhookClient(bf.scid, bf.scto);
+  client.sendSlackMessage({
+  'username': user,
+  'attachments': [{
+    'pretext': msg,
+    'color': '#000',
+    'footer': 'Powered by Keystroke',
+    'ts': Date.now() / 1000
+  }]
+})
 };
