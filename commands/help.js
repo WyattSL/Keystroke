61548b4c7eaca7ca5e7894bc36adcffb506a0608
Glitch (keystroke) <none>
2019-10-24 00:22:51 +0000

    👕🚈 Checkpoint ./commands/say.js:1076878/55 ./bots/KeyTest.json:1076878/11 ./commands/help.js:1076878/295

--- commands/help.js
+++ commands/help.js
@@ -5,8 +5,5 @@ const perms = require('../perms.js');
 const global = require('../global.js');
 
 exports.run = function(n, c, msg) {
-  if (!perms.checkPerm("MANAGE_MESSAGES", msg.member, msg.channel)) return;
-  var tosay = msg.content.slice(5, msg.content.length);
-  msg.delete();
-  msg.channel.send(tosay);
-};
+  msg.channel.send(global.help(n, c))
+}
