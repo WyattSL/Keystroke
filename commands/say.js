60065dde4fc5634979a9c79666cdacda645ad9ee
Glitch (keystroke) <none>
2019-10-24 00:10:50 +0000

    🏤🛎 Checkpoint ./commands/say.js:1076878/235

--- commands/say.js
+++ commands/say.js
@@ -1,4 +1,11 @@
+//basic api functions
+const { Client, RichEmbed } = require('discord.js');
+const SQL = require('../mysql.js');
+const perms = require('../perms.js');
+const global = require('../global.js');
+
 exports.run = function(n, c, msg) {
+  if (!perms.checkPerms)
   var tosay = msg.content.slice(5, msg.content.length);
   msg.delete();
   msg.channel.send(tosay);
