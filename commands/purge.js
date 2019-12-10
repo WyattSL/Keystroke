db4f443a44824015ac0e7d6731c5956fbcbdeacf
Glitch (keystroke) <none>
2019-12-10 01:02:19 +0000

    🙌🙉 Checkpoint ./commands/purge.js:1076878/1157

---
+++ commands/purge.js
@@ -0,0 +1,11 @@
+//basic api functions
+const { Client, RichEmbed } = require('discord.js');
+const SQL = require('../mysql.js');
+const perms = require('../perms.js');
+const global = require('../global.js');
+
+exports.run = function(n, c, msg) {
+  var target = msg.mentions.users.first();
+  var silent = msg.content.includes(`-s`);
+  var num = msg.content.split("-num ").split(" ")[0]
+};
