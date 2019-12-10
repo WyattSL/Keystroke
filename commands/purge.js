eb36c0d8543c4e41a2a413d2bf8ad26ab5095d54
Glitch (keystroke) <none>
2019-12-10 01:26:18 +0000

    👝😋 Checkpoint ./commands/purge.js:1076878/274 ./package.json:1076878/2828

--- commands/purge.js
+++ commands/purge.js
@@ -8,4 +8,8 @@ exports.run = function(n, c, msg) {
   var target = msg.mentions.users.first();
   var silent = msg.content.includes(`-s`);
   var num = msg.content.split("-num ").split(" ")[0];
+  var channel = msg.mentions.channels.first();
+  if (!channel) channel = msg.channel;
+  if (!num) num = 1000;
+  if (!target) target
 };
