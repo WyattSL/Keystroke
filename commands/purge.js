26ec440a1ee3b7296395d8b32b38e0a9b9adce56
WyattL <Wyattlipscomb10@gmail.com>
2019-12-10 18:21:35 -0500

    purge command : rough, testing

--- commands/purge.js
+++ commands/purge.js
@@ -5,11 +5,29 @@ const perms = require('../perms.js');
 const global = require('../global.js');
 
 exports.run = function(n, c, msg) {
-  var target = msg.mentions.users.first();
-  var silent = msg.content.includes(`-s`);
-  var num = msg.content.split("-num ").split(" ")[0];
-  var channel = msg.mentions.channels.first();
-  if (!channel) channel = msg.channel;
-  if (!num) num = 1000;
-  if (!target) target
+    var target = msg.mentions.users.first();
+    var silent = msg.content.includes(`-s`);
+    var num = msg.content.split("-num ").split(" ")[0];
+    if (!num) num = 999;
+    var channel = msg.mentions.channels.first();
+    if (!channel) channel = msg.channel;
+    if (!num) num = 1000;
+    channel.fetchMessages({ 'limit': num }).then(messages => {
+        var deleted = 0;
+        var i;
+        for (i = 0; i < messages.array().length; i++) {
+            var msg = messages.array()[i];
+            if (!target || msg.author.id == target.id) {
+                msg.delete();
+                deleted++
+            }
+        };
+        if (!silent) {
+            if (deleted == 0) {
+                msg.channel.send(`No messages deleted.`);
+            } else {
+                msg.channel.send(`Deleted ${deleted} messages.`);
+            }
+        }
+    });
 };
