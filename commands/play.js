df7bdbef4a4e350637d704629228ed9ae848e5ab
Wyatt Lipscomb <35582368+WyattSL@users.noreply.github.com>
2020-03-06 13:10:21 -0500

    Update play.js

---
+++ commands/play.js
@@ -0,0 +1,29 @@
+const ytdl = require('ytdl');
+const ytsr = require('ytsr');
+
+exports.run = function(name, client, msg) {
+  var args = msg.content.split(" ").shift();
+  var q = args.join(" ");
+  if (client.playingMusic) {
+    msg.channel.send(":x: I am busy playing music in another channel, sorry.");
+    return false;
+  } elseif (!msg.member.voiceChannel) {
+    msg.channel.send(":x: You are not in a music channel!");
+    return false;
+  } else {
+    ytsr.searchResults(q, {limit: 1}, function(err, results) {
+      if (err) {
+        msg.channel.send("An error has occured. Please notify me (WyatL#347) with info YTSR:RESULTERROR:" + err);
+        console.error(err);
+        return false;
+      } elseif (!results) {
+      
+      } else {
+      
+      }
+    });
+  }
+};
+
+exports.usage = "play (id/search query: youtube)";
+exports.description = "Play a youtube video. Must be categorized as 'music'.";
